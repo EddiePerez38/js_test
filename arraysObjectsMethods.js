2154915ffdc60e7641815195f5dee1d75e9b2e81
Eddie Perez <eddie.e.perez4@gmail.com>
2019-07-15 13:07:26 -0700

    tip calculator

--- arraysObjectsMethods.js
+++ arraysObjectsMethods.js
@@ -27,8 +27,24 @@ Arrays
 //   console.log(john.indexOf(1990));
 
 
-function tipCalculator(bill1, bill2, bill3) {
-    if (bill1 >= 50 && bill1 <= 199){
-        console.log('The tip is ' + bill1 * .20 + ' and the bills is ' + bill1 * 1.20 );
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50 ){
+        percentage = .2;
     }
+    else if (bill >= 50 && bill < 200){
+        percentage = .15;
+    }
+    else {
+        percentage = .1;
+    }
+    return percentage * bill;
 }
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]), 
+            tipCalculator(bills[1]), 
+            tipCalculator(bills[2])];
+var findValues = [bills[0] + tips[0], bills[1] + tips[1], bills[0] + tips[0]]
+
+console.log(tips);
